b5f4a25fc7c42c2a7948209be7a80301084cf519
HesusG <hesus.cobos@gmail.com>
2023-10-11 18:30:05 -0400

    - adding getting orders and drivers, db setup

--- orders_site/server.js
+++ orders_site/server.js
@@ -30,51 +30,214 @@ app.listen(HTTP_PORT, () => {
   console.log(`Server is running at http://localhost:${HTTP_PORT}`);
 });
 
-const orderSchema = new mongoose.Schema({
-  customerName: {
+const orderSchema = new mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    deliveryAddress: {
+      type: String,
+      required: true,
+    },
+    itemsOrdered: {
+      type: [
+        {
+          name: String,
+          quantity: Number,
+          price: Number,
+        },
+      ],
+      required: true,
+    },
+    orderDateTime: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["RECEIVED", "READY FOR DELIVERY", "IN TRANSIT", "DELIVERED"],
+      default: "RECEIVED",
+    },
+    orderConfirmation: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false }
+);
+
+const driverSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true,
   },
-  deliveryAddress: {
+  licensePlate: {
     type: String,
-    required: true,
-  },
-  itemsOrdered: {
-    type: [String],
-    required: true,
-  },
-  orderDateTime: {
-    type: Date,
-    default: Date.now,
-  },
-  status: {
-    type: String,
-    enum: ["RECEIVED", "READY FOR DELIVERY", "IN TRANSIT", "DELIVERED"],
-    default: "RECEIVED",
-  },
-  orderConfirmation: {
-    type: Number,
-    unique: true,
     required: true,
   },
 });
 
 const Order = mongoose.model("Order", orderSchema);
+const Driver = mongoose.model("Driver", driverSchema);
 
-router.get("/current-orders", async (req, res) => {
+const setupDatabase = async () => {
   try {
-    // Fetch current orders from the database
-    const currentOrders = await Order.find({ status: "RECEIVED" })
-      .sort({ orderDateTime: -1 })
-      .exec();
-
-    // Render the "currentOrders" view and pass data
-    res.render("currentOrders", {
-      layout: "layout",
-      orders: currentOrders,
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    const orderCount = await Order.countDocuments();
+
+    if (orderCount === 0) {
+      const timestamp = new Date();
+
+      await Order.insertMany([
+        {
+          customerName: "John Doe",
+          deliveryAddress: "123 Main Street, City, State",
+          itemsOrdered: [
+            {
+              name: "Taco",
+              quantity: 2,
+              price: 2.99,
+            },
+          ],
+          orderDateTime: new Date(),
+          status: "RECEIVED",
+          orderConfirmation: "abc123",
+        },
+        {
+          customerName: "Jane Smith",
+          deliveryAddress: "456 Elm Street, Town, State",
+          itemsOrdered: [
+            {
+              name: "Taco",
+              quantity: 1,
+              price: 2.99,
+            },
+          ],
+          orderDateTime: new Date(Date.now() + 1 * 60 * 1000),
+          status: "READY FOR DELIVERY",
+          orderConfirmation: "def456",
+          assignedTo: "XYZ-123",
+        },
+        {
+          customerName: "Alice Johnson",
+          deliveryAddress: "789 Oak Avenue, Village, State",
+          itemsOrdered: [
+            {
+              name: "Burger",
+              quantity: 3,
+              price: 4.99,
+            },
+          ],
+          orderDateTime: new Date(Date.now() + 2 * 60 * 1000),
+          status: "IN TRANSIT",
+          orderConfirmation: "ghi789",
+          assignedTo: "ABC-789",
+        },
+        {
+          customerName: "Bob Wilson",
+          deliveryAddress: "101 Pine Street, Hamlet, State",
+          itemsOrdered: [
+            {
+              name: "Burger",
+              quantity: 2,
+              price: 4.99,
+            },
+          ],
+          orderDateTime: new Date(Date.now() + 3 * 60 * 1000),
+          status: "DELIVERED",
+          orderConfirmation: "jkl101",
+          assignedTo: "DEF-456",
+        },
+        {
+          customerName: "Eve Johnson",
+          deliveryAddress: "789 Oak Avenue, Village, State",
+          itemsOrdered: [
+            {
+              name: "Taco",
+              quantity: 3,
+              price: 2.99,
+            },
+          ],
+          orderDateTime: new Date(Date.now() + 4 * 60 * 1000),
+          status: "READY FOR DELIVERY",
+          orderConfirmation: "mno456",
+          assignedTo: "",
+        },
+        {
+          customerName: "Charlie Brown",
+          deliveryAddress: "101 Pine Street, Hamlet, State",
+          itemsOrdered: [
+            {
+              name: "Taco",
+              quantity: 2,
+              price: 2.99,
+            },
+          ],
+          orderDateTime: new Date(Date.now() + 5 * 60 * 1000),
+          status: "IN TRANSIT",
+          orderConfirmation: "pqr101",
+          assignedTo: "OPQ-123",
+        },
+      ]);
+
+      console.log("Orders collection was created and prepopulated with data");
+    } else {
+      console.log("Orders collection already exists, no prepopulation needed");
+    }
   } catch (error) {
-    console.error("Error fetching current orders:", error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error setting up orders collection: " + error.message);
+  }
+};
+
+setupDatabase();
+app.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ orderDateTime: 1 }).lean().exec();
+    const isEmptyOrders = orders.length === 0;
+
+    for (const order of orders) {
+      order.formattedOrderDateTime = formatDate(order.orderDateTime);
+      order.orderTotal = calculateOrderTotal(order.itemsOrdered);
+      order.isReceived = order.status === "RECEIVED";
+      order.isDelivered = order.status === "DELIVERED";
+
+      const matchedDriver = await Driver.findOne({
+        licensePlate: order.assignedTo,
+      })
+        .lean()
+        .exec();
+
+      if (matchedDriver) {
+        order.driverName = matchedDriver.name;
+        order.driverLicensePlate = matchedDriver.licensePlate;
+      }
+    }
+
+    return res.render("orders", { orders, isEmptyOrders });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
   }
 });
+
+const formatDate = (orderDateTime) => {
+  return orderDateTime;
+};
+
+const calculateOrderTotal = (itemsOrdered) => {
+  let total = 0;
+  for (const item of itemsOrdered) {
+    total += item.quantity * item.price;
+  }
+  return total;
+};
